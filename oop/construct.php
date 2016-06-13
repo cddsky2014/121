c26df6682a67db5cb1c8c7c470b0bc0e695eea01
cddsky <cddsky@qq.com>
2016-06-13 11:40:32 +0800

    add

--- oop/construct.php
+++ oop/construct.php
@@ -1,14 +1,21 @@
 <?php
 class A{
     public $x=1;
-    public $y=2;    
-    public function __construct() {
-       echo $this->x;//$a->x;
-       echo $this->y;
-    }
+    public $y=2;
     public function add(){
            echo "add";       
-    } 
+    }     
+    public function test(){        
+        $this->add();
+    }   
+       public function __construct($a,$b) {
+        echo $a+$b; 
+    }
+    public function __destruct() {
+        echo __FUNCTION__;
+    }
 }
-$a = new A;
+$a = new A(1,2);
 //$a->__construct();
+//$a->add();
+//$a->test();
