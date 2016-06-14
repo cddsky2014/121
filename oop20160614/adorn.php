b0fd563c0b2926ddc51e6b87110acba11a85a28c
cddsky <cddsky@qq.com>
2016-06-14 11:15:36 +0800

    add

---
+++ oop20160614/adorn.php
@@ -0,0 +1,38 @@
+<?php
+/*
+    public 公共的
+    private 私有的
+    protected 受保护的
+ */
+class Guoxiaotian{
+    public $country = '宋';
+    protected $enemy = '段天德、完颜洪烈';
+    private $age = 44;
+    public function kungfu(){
+        echo '啸转天地';
+    }
+    public function get(){
+        echo $this->age;  
+        echo $this->enemy; 
+    }
+}
+$gxt = new Guoxiaotian;
+//echo $gxt->country;//宋
+//echo $gxt->get();//
+class Guojing extends Guoxiaotian{
+        
+}
+$gj = new Guojing;
+//echo $gj->enemy;
+echo $gj->get();
+
+/*          
+    修饰符      本类    子类    对象
+    public      yes     yes     yes
+    private     yes     no      no 
+    protected   yes     yes     no    
+ */
+
+
+
+
--- oop20160614/extends.php
+++ oop20160614/extends.php
@@ -1,5 +1,8 @@
 <?php
 class Guoxiaotian{
+    public function __construct() {
+        echo 'Guoxiaotian';
+    }
     public $country = '宋';
     public $enemy = '段天德、完颜洪烈';
     public $age = 44;
@@ -9,8 +12,13 @@ class Guoxiaotian{
 }
 //子类继承父类
 class Guojing extends Guoxiaotian{
+    public function __construct() {
+        parent::__construct();
+        echo 'Guojing';
+    }
     public $age = 18;//重写父类属性
     public function kungfu(){//重写父类方法
+        //parent::方法|属性 访问父类中的属性和方法
         parent::kungfu();//echo '啸转天地';
         echo '降龙十八掌';
     }    
