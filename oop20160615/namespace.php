15d68a15b7c7bf749c798305af960a7613ebe4d4
cddsky <cddsky@qq.com>
2016-06-15 08:58:12 +0800

    add

---
+++ oop20160615/namespace.php
@@ -0,0 +1,17 @@
+<?php
+namespace NS1{    
+    class A{
+        public $name = 'NS1 A';
+    
+
+}
+namespace NS2{
+    class A{
+        public $name = 'NS2 A';        
+    }
+    $a = new A;
+    echo $a->name;    
+    $a1 = new \NS1\A;    
+    echo $a1->name;
+}
+
